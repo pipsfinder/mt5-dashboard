08b3427b49af2298d1a53d0faae3aacb24dbf019
pipsfinder <roadnonumber@gmail.com>
2026-02-24 01:52:35 +0700

    Update autopush.bat

--- autopush.bat
+++ autopush.bat
@@ -2,7 +2,7 @@
 
 copy "C:\Users\ADM\AppData\Roaming\MetaQuotes\Terminal\Common\Files\trades.json" "C:\Users\ADM\Documents\mt5-dashboard\trades.json" /Y
 
-cd "C:\Users\ADM\Documents\mt5-dashboard"
+cd "C:\Users\ADM\Documents\GitHub\mt5-dashboard"
 
 git add trades.json
 git commit -m "auto update"

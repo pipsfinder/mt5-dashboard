23aa09ed58bc2ad96a37c84f33bf8fdd301e4377
pipsfinder <roadnonumber@gmail.com>
2026-02-24 02:37:43 +0700

    Update autopush.bat

--- autopush.bat
+++ autopush.bat
@@ -1,12 +1,28 @@
+@echo off
+
 :loop
+
+echo Copying trades.json...
 
 copy "C:\Users\ADM\AppData\Roaming\MetaQuotes\Terminal\Common\Files\trades.json" "C:\Users\ADM\Documents\GitHub\mt5-dashboard\trades.json" /Y
 
-cd "C:\Users\ADM\Documents\GitHub\mt5-dashboard"
+echo Switching to GitHub folder...
+
+cd /d "C:\Users\ADM\Documents\GitHub\mt5-dashboard"
+
+echo Adding file...
 
 git add trades.json
+
+echo Committing...
+
 git commit -m "auto update"
+
+echo Pushing...
+
 git push origin main --force
+
+echo Done. Waiting 5 seconds...
 
 timeout /t 5
 
